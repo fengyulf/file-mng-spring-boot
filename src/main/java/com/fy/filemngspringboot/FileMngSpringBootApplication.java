f2861e0945aa1491832e72df8c192b813ccb4498
fengyu <fengyu@163.com>
2019-11-29 10:32:41 +0800

    ces

---
+++ src/main/java/com/fy/filemngspringboot/FileMngSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fy.filemngspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileMngSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileMngSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fy/filemngspringboot/FileMngSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fy.filemngspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileMngSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
